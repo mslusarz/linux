86817ecb303a2dd5f354ccbe7cd98efb39819b2d
Marcin Slusarz <marcin.slusarz@gmail.com>
2012-11-03 21:44:56 +0100

    nvXX_ltcg_priv subdev conv
    @@
    struct nvc0_ltcg_priv *p;
    expression addr, mask, data;
    @@
    (
    - nv_rd08(p, addr)
    + nvc0_ltcg_rd08(p, addr)
    |
    - nv_rd16(p, addr)
    + nvc0_ltcg_rd16(p, addr)
    |
    - nv_rd32(p, addr)
    + nvc0_ltcg_rd32(p, addr)
    |
    - nv_wr08(p, addr, data)
    + nvc0_ltcg_wr08(p, addr, data)
    |
    - nv_wr16(p, addr, data)
    + nvc0_ltcg_wr16(p, addr, data)
    |
    - nv_wr32(p, addr, data)
    + nvc0_ltcg_wr32(p, addr, data)
    |
    - nv_mask(p, addr, mask, data)
    + nvc0_ltcg_mask(p, addr, mask, data)
    )

--- drivers/gpu/drm/nouveau/core/subdev/ltcg/nvc0.c
+++ drivers/gpu/drm/nouveau/core/subdev/ltcg/nvc0.c
@@ -34,11 +34,11 @@ static void
 nvc0_ltcg_subp_isr(struct nvc0_ltcg_priv *priv, int unit, int subp)
 {
 	u32 subp_base = 0x141000 + (unit * 0x2000) + (subp * 0x400);
-	u32 stat = nv_rd32(priv, subp_base + 0x020);
+	u32 stat = nvc0_ltcg_rd32(priv, subp_base + 0x020);
 
 	if (stat) {
 		nv_info(priv, "LTC%d_LTS%d: 0x%08x\n", unit, subp, stat);
-		nv_wr32(priv, subp_base + 0x020, stat);
+		nvc0_ltcg_wr32(priv, subp_base + 0x020, stat);
 	}
 }
 
@@ -48,7 +48,7 @@ nvc0_ltcg_intr(struct nouveau_subdev *subdev)
 	struct nvc0_ltcg_priv *priv = (void *)subdev;
 	u32 units;
 
-	units = nv_rd32(priv, 0x00017c);
+	units = nvc0_ltcg_rd32(priv, 0x00017c);
 	while (units) {
 		u32 subp, unit = ffs(units) - 1;
 		for (subp = 0; subp < priv->subp_nr; subp++)
@@ -59,7 +59,7 @@ nvc0_ltcg_intr(struct nouveau_subdev *subdev)
 	/* we do something horribly wrong and upset PMFB a lot, so mask off
 	 * interrupts from it after the first one until it's fixed
 	 */
-	nv_mask(priv, 0x000640, 0x02000000, 0x00000000);
+	nvc0_ltcg_mask(priv, 0x000640, 0x02000000, 0x00000000);
 }
 
 static int
@@ -75,8 +75,8 @@ nvc0_ltcg_ctor(struct nouveau_object *parent, struct nouveau_object *engine,
 	if (ret)
 		return ret;
 
-	priv->subp_nr = nv_rd32(priv, 0x17e8dc) >> 24;
-	nv_mask(priv, 0x17e820, 0x00100000, 0x00000000); /* INTR_EN &= ~0x10 */
+	priv->subp_nr = nvc0_ltcg_rd32(priv, 0x17e8dc) >> 24;
+	nvc0_ltcg_mask(priv, 0x17e820, 0x00100000, 0x00000000); /* INTR_EN &= ~0x10 */
 
 	nv_subdev(priv)->intr = nvc0_ltcg_intr;
 	return 0;
